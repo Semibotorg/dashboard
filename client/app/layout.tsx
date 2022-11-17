cd4752bd7d859cca2385d5d4f45401f09322cc62
MohamedAshraf <mohmed6002090@gmail.com>
2022-11-18 01:59:40 +0200

    commit

---
+++ client/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Semibot</title>
+        <link rel="icon" href="./favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
